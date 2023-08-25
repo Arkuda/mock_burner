d8771758266c91b84c2b31b5eb566c436649fc00
Andrey Kiryantsev <reyzor2142@gmail.com>
2023-08-25 21:09:00 +0400

    init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.10"
+    id("io.ktor.plugin") version "2.3.3"
+}
+
+group = "ru.kiryantsev"
+version = "0.0.1"
+
+application {
+    mainClass.set("ru.kiryantsev.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // server
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-serialization-gson-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+//    testImplementation("io.ktor:ktor-server-tests-jvm")
+//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    //client
+    implementation ("io.ktor:ktor-client-cio-jvm")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ru.kiryantsev.mock_burner"
